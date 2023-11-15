2476cc3f359abf45c043f4581ef0ee701b8f9953
olegtemek <olegtemek@gmail.com>
2023-11-15 11:44:13 +0600

    add link to psycho test

--- web/components/index/Condition.vue
+++ web/components/index/Condition.vue
@@ -14,7 +14,8 @@
           <h3>{{ $t(`Здоровье`) }}</h3>
           <div>
             <p>{{ status ? $t(`condition3_rec`) : $t(`condition3`) }}</p>
-            <a href="#">{{ $t(`Пройти психологический тест`) }}<svg xmlns="http://www.w3.org/2000/svg" width="25"
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSdCqingV029sRrCr5-GRFYn8nMC0qs12kR1zdIqYFSP3Fim2A/viewform?usp=sf_link"
+              target="_blank">{{ $t(`Пройти психологический тест`) }}<svg xmlns="http://www.w3.org/2000/svg" width="25"
                 height="16" viewBox="0 0 25 16" fill="none">
                 <path
                   d="M24.7071 8.7071C25.0976 8.31658 25.0976 7.68342 24.7071 7.29289L18.3431 0.928931C17.9526 0.538406 17.3195 0.538406 16.9289 0.928931C16.5384 1.31946 16.5384 1.95262 16.9289 2.34314L22.5858 8L16.9289 13.6569C16.5384 14.0474 16.5384 14.6805 16.9289 15.0711C17.3195 15.4616 17.9526 15.4616 18.3431 15.0711L24.7071 8.7071ZM8.74228e-08 9L24 9L24 7L-8.74228e-08 7L8.74228e-08 9Z"
